b5356efaab05fb91b5da8096ec3614310fede515
yuzai <a545905403@163.com>
2025-03-08 14:50:06 +0800

    fix: restore tailwindcss config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
   base: '/my-tinypng/',  // 修改为你的仓库名
 })
