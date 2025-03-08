4f214906837d0eb0dca242a074610e8a39aca6db
yuzai <a545905403@163.com>
2025-03-08 14:49:17 +0800

    fix: update vite config

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-  base: '/tinypng/',
+  plugins: [react()],
+  base: '/my-tinypng/',  // 修改为你的仓库名
 })
